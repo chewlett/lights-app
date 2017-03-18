6084f351816bc961cbc4dbfe1175c33d16b377d0
Curtis Hewlett <chewlett2@my.bcit.ca>
2017-03-18 15:26:04 -0700

    Converted light services to promises and added all light front end

--- server.js
+++ server.js
@@ -8,20 +8,42 @@ var Light = require('./models/light');
 var lightService = require('./services/lightsService');
 
 var app = express()
+
+var viewEngine = require('ejs-locals');
+app.engine('ejs', viewEngine);
+app.set('view engine', 'ejs');
+
 app.set('port', config.port);
 
 app.use(express.static(path.join(__dirname, 'client')));
 
 app.get('/', (req, res, next) => {
-    res.render('index.html')
+    let allLights = null;
+    Light.getAllLights(function(err, lights) {
+        allLights = lights;
+        res.render('index', {
+            title : "All Lights", 
+            data: allLights
+        });
+    });
+})
+app.get('/toggle/:id', function(req, res, next) {
+    let id = req.params.id;
+    Light.getLightById(id, function(err, light) {
+        if (err) {
+            res.send(err);
+        }
+        // let result = lightService.toggleLight(light);
+        // console.log(result);
+        lightService.toggleLight(light).then(function (response) {
+            res.send(response);
+        })
+        // res.send(lightService.toggleLight(light));
+        // res.send("done");
+    });
 })
 
 app.listen(app.get("port"), function() {
     console.log("Server started on port: " + app.get("port"));
-    Light.getAllLights(function(err, lights) {
-        console.log(lights);
-        lightService.toggleLight(lights[0]);
-    })
-    
 });
 
--- services/lightsService.js
+++ services/lightsService.js
@@ -3,51 +3,67 @@ var Light = require('../models/light');
 var config = require('../config/main');
 
 module.exports.toggleLight = function(light) {
-    if (light.type == 'hue') {
-        toggleHue(light);
-    }
-    else if (light.type == 'wemo') {
-        toggleWemo(light);
-    }
+	return new Promise(function (fulfill, reject) {
+		if (light.type == 'hue') {
+			toggleHue(light).then(res => {fulfill(res)});
+		}
+		else if (light.type == 'wemo') {
+			toggleWemo(light).then(res => {fulfill(res)});
+		}
+	});    
 }
 
 function toggleHue(light) {
-    request('http://' + config.hueIP + '/api/' + config.hueKey + '/lights/' + light.number, 
-	(err, res, body) => {
-		if (res.statusCode == 200) {
-			console.log("toggling")
-			try {
-				let info = JSON.parse(body);
-				console.log(info.state.on)
-				if (info.state.on) {
-					hueOff(light);
+	return new Promise(function (fulfill, reject) {
+		request('http://' + config.hueIP + '/api/' + config.hueKey + '/lights/' + light.number, 
+		(err, res, body) => {
+			if (res.statusCode == 200) {
+				try {
+					let info = JSON.parse(body);
+					if (info.state.on) {
+						hueOff(light).then(function (res) {
+							fulfill(res);
+						});
+					}
+					else {
+						hueOn(light).then(function (res) {
+							fulfill(res);
+						});
+					}
 				}
-				else {
-					hueOn(light);
+				catch (e) {
+					console.log("can't parse" + e)			
 				}
 			}
-			catch (e) {
-				console.log("can't parse" + e)			
-			}
-		}
-    });
+		});
+	});
 }
 
 function hueOn(light) {
-    request({
-        url: 'http://' + config.hueIP + '/api/' + config.hueKey + '/lights/'+light.number+'/state',
-        method: 'PUT',
-        json: {"on": true}
-    }, (err, res, body) => {
-		console.log(body);
-	});  
+	return new Promise(function (fulfill, reject) {
+		request({
+			url: 'http://' + config.hueIP + '/api/' + config.hueKey + '/lights/'+light.number+'/state',
+			method: 'PUT',
+			json: {"on": true}
+		}, (err, res, body) => {
+			if (err) {
+				reject(err);
+			}
+			else { fulfill(body)}
+		}); 
+	});
 }
 function hueOff(light) {
-    request({
-        url: 'http://' + config.hueIP + '/api/' + config.hueKey + '/lights/'+light.number+'/state',
-        method: 'PUT',
-        json: {"on": false}
-    }, (err, res, body) => {
-		console.log(body);
-	}) 
+	return new Promise(function (fulfill, reqject) {
+		request({
+			url: 'http://' + config.hueIP + '/api/' + config.hueKey + '/lights/'+light.number+'/state',
+			method: 'PUT',
+			json: {"on": false}
+		}, (err, res, body) => {
+			if (err) {
+				reject(err);
+			}
+			else { fulfill(body)}
+		});
+	});
 }
